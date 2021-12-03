256662817122165eb8e570bf3bac688efba4dc2a
Ernestcollins69 <55945130+Ernestcollins69@users.noreply.github.com>
2021-12-03 13:39:28 +0300

    Add files via upload

---
+++ assessment_table.sql
@@ -0,0 +1,9 @@
+--Assessment table
+ CREATE TABLE Assessment_table
+ (Farm_location VARCHAR(20) NOT NULL,
+ vegetation_cover VARCHAR(20),
+  watersource_proximity INTEGER NOT NULL,--  IN KM
+  hive_security VARCHAR(20) NOT NULL);
+  
+  SELECT * FROM Assessment_table;
+  
